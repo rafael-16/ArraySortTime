12fd996b13688ba1c28acda99b2eba60ae41d426
Рафаэль Мухамедьяров <Rafael17IS16@gmail.com>
2024-04-20 14:28:18 +0300

    Add project files.

---
+++ ArraySortTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySortTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк:");
+            int row = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество столбцов");
+            int col = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
